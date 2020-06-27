bbd791465c1812e275ba46091713b5ad7f2e1430
Volker Schmitt <volker.schmitt.88@icloud.com>
2020-06-27 19:24:19 +0200

    Initial Commit

---
+++ Shared/UniversalSwiftUISidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalSwiftUISidebarApp.swift
+//  Shared
+//
+//  Created by Volker Schmitt on 27.06.20.
+//
+
+import SwiftUI
+
+@main
+struct UniversalSwiftUISidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
